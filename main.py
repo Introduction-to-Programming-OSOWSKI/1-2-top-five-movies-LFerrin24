754c7400f47ac1b3c841ddec3fd1a52b391bb2cf
LFerrin24 <825875@mystma.org>
2023-03-24 14:33:21 -0500

    6

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #WRITE YOUR CODE IN THIS FILE
 def topFiveMovies(a, b, c, d, e):
+
     return(a + b + c + e)
+
 topFiveMovies("Spider Man", "Iron Man", "Doctor Strange", "The Hulk", "Captain America")
 
 
